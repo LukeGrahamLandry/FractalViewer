00a07dd9731c5e247552eb3ff1d5e31e49b5cecf
LukeGrahamLandry <graham11000@gmail.com>
2023-06-22 23:13:42 -0400

    Initial Commit

---
+++ Mandelbrot/MandelbrotView.h
@@ -0,0 +1,12 @@
+//
+//  MandelbrotView.h
+//  Mandelbrot
+//
+//  Created by Luke Graham Landry on 2023-06-22.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MandelbrotView : ScreenSaverView
+
+@end
